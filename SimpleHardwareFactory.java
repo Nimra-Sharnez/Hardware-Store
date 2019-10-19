3cc704f218bfc12e318f2b517e2f2ddddccdc9f3
Nish6496 <36617684+Nimra-Sharnez@users.noreply.github.com>
2019-10-18 23:38:34 -0600

    Add files via upload

---
+++ SimpleHardwareFactory.java
@@ -0,0 +1,20 @@
+package HardwareStoreSimulation;
+
+import java.util.List;
+
+import HardwareStoreSimulation.Tool.PaintingTool;
+import HardwareStoreSimulation.Tool.PlumbingTool;
+import HardwareStoreSimulation.Tool.Tool;
+import HardwareStoreSimulation.Tool.WoodworkTool;
+import HardwareStoreSimulation.Tool.YardworkTool;
+
+
+public class SimpleHardwareFactory { 
+	public Tool createTool(String type) { //factory method
+		Tool tool = null;
+
+		return tool;
+	}
+
+}
+
